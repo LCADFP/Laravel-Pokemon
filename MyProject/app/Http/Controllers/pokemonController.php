e94517e80a5b689c415d6a7f8b8413c8d18c6177
dafp18 <dafp18@hotmail.com>
2019-03-08 17:31:02 -0500

    Trabajando con componentes de vue
    se agrega un nuevo componente para los pokemones, y se agrega otro para el spiner de loading, hay que ejecutar npm install, y los comandos de ejecucion en tieempo real para visualizar los cambois son npm run dev, y npm run watch.

--- MyProject/app/Http/Controllers/pokemonController.php
+++ MyProject/app/Http/Controllers/pokemonController.php
@@ -7,7 +7,15 @@ use Illuminate\Http\Request;
 class pokemonController extends Controller
 {
     //
-    public function index(){
+    public function index(Request $request){
+        if($request->ajax()){
+            return response()->json([
+                ['id' => 1, 'nombre' => 'Pikachu', 'descripcion' => 'Tipo trueno'],
+                ['id' => 2, 'nombre' => 'Squirtle', 'descripcion' => 'Tipo agua'],
+                ['id' => 3, 'nombre' => 'Charizar', 'descripcion' => 'Tipo fuego'],
+
+            ]);
+        }
         return view('pokemons.index');
     }
 }
--- MyProject/resources/js/components/PokemonsComponent.vue
+++ MyProject/resources/js/components/PokemonsComponent.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="row" >
+        <spiner v-show="loading"></spiner>
         
         <div class="col-sm" v-for="pokemon in pokemons">
             <div class="card text-center" style="width: 18rem; margin-top: 70px">
             <img style="height:100px; width:100px; background-color: #EFEFEF; margin:15px;" src="imagenes/" class="card-img-top rounded-circle mx-auto d-block" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">PokemonNombre</h5>
-                    <p class="card-text">PokemonDescrip</p>
+                    <h5 class="card-title">{{ pokemon.nombre }}</h5>
+                    <p class="card-text">{{ pokemon.descripcion }}</p>
                 <a href="/maestros/" class="btn btn-primary">Detalles</a>
                 </div>
             </div>
@@ -20,16 +21,18 @@
  export default {
      data(){
          return {
-             pokemons: [
-                 {id: 1, nombre: "Pikachu"},
-                 {id: 2, nombre: "Squirtle"},
-                 {id: 3, nombre: "Charizard"}
-
-             ]
+             pokemons: [],
+             loading: true
          }
      },
         mounted() {
-            console.log('Component mounted.')
+            axios
+                .get('http://localhost:8000/pokemons')
+                .then((res) => {
+                    this.pokemons = response.data
+                    this.loading = false
+                })
+                
         }
     }
 
